b61231953d8ddf9a78b572b4f260d0d6e8d994eb
Moe Arkar <ejone1313@gmail.com>
2026-04-30 17:47:18 +0630

    feat: generate O3 optimized assembly to observe register allocation

---
+++ phase_0_assembly/math_optimized.s
@@ -0,0 +1,27 @@
+	.file	"main.c"
+	.text
+	.p2align 4
+	.globl	add
+	.def	add;	.scl	2;	.type	32;	.endef
+	.seh_proc	add
+add:
+	.seh_endprologue
+	leal	(%rcx,%rdx), %eax
+	ret
+	.seh_endproc
+	.section	.text.startup,"x"
+	.p2align 4
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	subq	$40, %rsp
+	.seh_stackalloc	40
+	.seh_endprologue
+	call	__main
+	xorl	%eax, %eax
+	addq	$40, %rsp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev13, Built by MSYS2 project) 15.2.0"
