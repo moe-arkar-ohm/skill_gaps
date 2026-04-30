9b38ec74ea1a799faa04c046d0c7da91db71cdbd
Moe Arkar <ejone1313@gmail.com>
2026-04-30 16:51:33 +0630

    chore: initial commit inspecting C to Assembly

---
+++ phase_0_assembly/main.c
@@ -0,0 +1,8 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int result = add(5, 7);
+    return 0;
+}
---
+++ phase_0_assembly/main.s
@@ -0,0 +1,42 @@
+	.file	"main.c"
+	.text
+	.globl	add
+	.def	add;	.scl	2;	.type	32;	.endef
+	.seh_proc	add
+add:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	16(%rbp), %edx
+	movl	24(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	ret
+	.seh_endproc
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$7, %edx
+	movl	$5, %ecx
+	call	add
+	movl	%eax, -4(%rbp)
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev13, Built by MSYS2 project) 15.2.0"
